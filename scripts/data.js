92c2d44384c206fd2a49f0e81126aa9057f4d059
Felipe Valentin Nascimento <felipe.val.nascimento@gmail.com>
2019-03-11 22:06:01 -0300

    New js file for the locations data

---
+++ scripts/data.js
@@ -0,0 +1,9 @@
+// locations data
+var locations = [
+  {title: 'Florianopolis', location: {lat: -27.594517, lng: -48.535801}},
+  {title: 'Federal University of Santa Catarina', location: {lat: -27.600420, lng: -48.519201}},
+  {title: 'Hercilio Luz bridge', location: {lat: -27.593741, lng: -48.565648}},
+  {title: 'Federal Institute of Santa Catarina', location: {lat: -27.594017, lng: -48.542968}},
+  {title: 'Public Market of Florianopolis', location: {lat: -27.596936, lng: -48.552785}},
+  {title: 'Morro da Cruz belvedere', location: {lat: -27.588502, lng: -48.533559}}
+]
